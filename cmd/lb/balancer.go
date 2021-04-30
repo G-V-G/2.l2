aa75bec60ec037d6100c145b0fbb216fbffc7115
Andrew1407 <endry1407@gmail.com>
2021-04-30 13:38:12 +0300

    travis dot file added

--- cmd/lb/balancer.go
+++ cmd/lb/balancer.go
@@ -19,7 +19,7 @@ var (
 	port         = flag.Int("port", 8090, "load balancer port")
 	timeoutSec   = flag.Int("timeout-sec", 3, "request timeout time in seconds")
 	https        = flag.Bool("https", false, "whether backends support HTTPs")
-	traceEnabled = flag.Bool("trace", true, "whether to include tracing information into responses")
+	traceEnabled = flag.Bool("trace", false, "whether to include tracing information into responses")
 	timeout      = time.Duration(*timeoutSec) * time.Second
 	serversPool  = []string{
 		"server1:8080",
